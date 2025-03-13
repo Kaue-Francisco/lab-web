f38baec9a9ca2423a2c256d231c550157b306854
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-03-12 23:31:05 -0300

    subindo os arquivos referente a atividade 02

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def delete_user(id):
 
 ################################################################################
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
